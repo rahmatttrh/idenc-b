f2b6613bb3b8ba9729fedff8496fb56f3cdc1bad
Rahmat H <rahmattrust@gmail.com>
2025-04-11 09:04:59 +0700

    change on server

--- app/Http/Controllers/CutiController.php
+++ app/Http/Controllers/CutiController.php
@@ -274,6 +274,7 @@ class CutiController extends Controller
 
       try {
          // Excel::import(new CargoItemImport($parent->id), $req->file('file-cargo'));
+         // dd($fileName);
          Excel::import(new CutiImport, public_path('CutiData/' . $fileName));
       } catch (Exception $e) {
          return redirect()->back()->with('danger', 'Import Failed ' . $e->getMessage());
@@ -303,10 +304,24 @@ class CutiController extends Controller
       $cuti = Cuti::find($cuti);
       // dd($cuti->end);
       $contract = Contract::find($cuti->employee->contract_id);
+      
       if ($cuti->expired != null) {
-         if ($cuti->expired < $today) {
+         
+         $expired = Carbon::create($cuti->expired);
+         // if(auth()->user()->hasRole('Administrator')){
+            
+         //    dd($today->format('Y-m-d'));
+         // }
+         
+         if ($cuti->expired > $today->format('Y-m-d')) {
             $extend = $cuti->extend;
+            // if(auth()->user()->hasRole('Administrator')){
+            
+            //    dd($extend);
+            // }
+            
          } else {
+           
             $extend = 0;
          }
       } else {

e94eb219c774ff1ca1a001866d26209db0e8a0e2
Rahmat Hidayat <rahmattrust@gmail.com>
2025-07-15 10:11:04 +0700

    fix data table

--- resources/views/components/absence/date.blade.php
+++ resources/views/components/absence/date.blade.php
@@ -1,10 +1,13 @@
 @if ($absence->type == 5 || $absence->type == 10)
       @if (count($absence->details) > 0)
             @if (count($absence->details) > 1)
-                  {{count($absence->details)}} Hari
+                  {{-- {{count($absence->details)}} Hari --}}
+                  @foreach ($absence->details  as $item)
+                  {{$item->date}} ,
+                  @endforeach
                   @else
                   @foreach ($absence->details  as $item)
-                  {{formatDate($item->date)}} 
+                  {{$item->date}} 
                   @endforeach
             @endif
             
@@ -13,5 +16,5 @@
       @endif
    {{-- {{count($absence->details)}} Hari --}}
       @else
-      {{formatDate($absence->date)}}
+      {{$absence->date}}
 @endif

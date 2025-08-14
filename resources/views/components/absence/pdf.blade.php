ac54f90fc652bda3b7c2c57e7361da88a5d967a5
Rahmat H <rahmattrust@gmail.com>
2025-08-14 16:22:20 +0700

    server

--- resources/views/components/absence/pdf.blade.php
+++ resources/views/components/absence/pdf.blade.php
@@ -910,8 +910,8 @@
          
          {{-- {{$absenceemp->doc}} --}}
          @if ($absenceemp->doc != null)
-            {{-- <img width="100%" src="/storage/{{$absenceemp->doc}}" alt=""> --}}
-            <iframe  src="/storage/{{$absenceemp->doc}}" style="width:100%; height:570px;" frameborder="0"></iframe>
+            <img width="100%" src="/storage/{{$absenceemp->doc}}" alt="">
+            <iframe  src="/storage/{{$absenceemp->doc}}" style="width:100%; height:570px;" frameborder="0" allowfullscreen ></iframe>
          @endif
          
 
